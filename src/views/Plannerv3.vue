158769d3b9a812e08662a286416247478488bdea
Robin <r.m.p.linde@students.uu.nl>
2020-09-22 16:15:55 +0200

    Add auto generation of pcp_input feature

--- src/views/Plannerv3.vue
+++ src/views/Plannerv3.vue
@@ -84,51 +84,49 @@
         <v-btn class="mt-4" text @click="e6 = 3">Previous</v-btn>
       </v-stepper-content>
 
-      <v-stepper-step :complete="e6 > 5" step="5">Configre parameters</v-stepper-step>
+      <v-stepper-step :complete="e6 > 5" step="5">Configure parameters</v-stepper-step>
       <v-stepper-content step="5">
         <!-- <p>Two planning algorithm were detected for your application type, configure the parameters below:</p> -->
         <!-- <p> Select preffered cloud provider </p> -->
-        <p>Available vms for your selected cloud proivder</p>
-        <v-simple-table height="300px">
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">id</th>
-                <th class="text-left">num_cpus</th>
-                <th class="text-left">mem_size</th>
-                <th class="text-left">disk_size</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="item in vms" :key="item.id">
-                <td>{{ item.id }}</td>
-                <td>{{ item.num_cpus }}</td>
-                <td>{{ item.mem_size }}</td>
-                <td>{{ item.disk_size }}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
+        <p> Do you want to generate a performance model? If you have your own you can skip this step.</p>
+        <v-btn class="justify-start" color="green" dark @click="generatePCPInput">Generate</v-btn>
+        <v-flex xs12 offset-xs8>
+        <!-- <v-file-input
+          multiple
+          label="Insert price model"
+          accept=".yaml, .yml"
+          v-model="pcp_price_model_file"
+        ></v-file-input> -->
+        <v-btn color="primary" @click="e6 = 6">Continue</v-btn>
+        <v-btn text @click="e6 = 4">Previous</v-btn>
+        </v-flex>
 
-        <v-file-input
+        <v-dialog v-model="dialog_input" max-width="290">
+          <v-card>
+            <v-card-title class="headline">Success</v-card-title>
+
+            <v-card-text>Input file has been successfully generated</v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn color="green darken-1" text @click="() => {dialog_input = false; e6 = 6}">Proceed</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+
+      </v-stepper-content>
+
+      <v-stepper-step step="6">Configure QoS demands</v-stepper-step>
+      <v-stepper-content step="6">
+          <v-file-input
           multiple
           label="Insert performance model"
           accept=".yaml, .yml"
           v-model="pcp_performance_file"
         ></v-file-input>
-        <v-file-input
-          multiple
-          label="Insert price model"
-          accept=".yaml, .yml"
-          v-model="pcp_price_model_file"
-        ></v-file-input>
-        <v-btn color="primary" @click="e6 = 6" :disabled="pcp_performance_file === null">Continue</v-btn>
-        <v-btn text @click="e6 = 4">Previous</v-btn>
-      </v-stepper-content>
-
-      <v-stepper-step step="6">Configure QoS demands</v-stepper-step>
-      <v-stepper-content step="6">
-        <v-form>
+        <!-- <v-form>
           <v-container>
             <v-row>
               <v-col cols="12" sm="6" md="3">
@@ -140,11 +138,11 @@
               </v-col>
             </v-row>
           </v-container>
-        </v-form>
+        </v-form> -->
         <v-btn
           color="primary"
           @click="getToscaViaFiles"
-          :disabled="deadline === ''"
+          :disabled="pcp_performance_file === null"
           :loading="loading"
         >Generate</v-btn>
         <v-btn text @click="e6 = 5">Previous</v-btn>
@@ -302,8 +300,9 @@ export default {
     loading: false,
     continue_button1: false,
     dialog: false,
+    dialog_input: false,
     stepper_visible: true,
-    e6: 4,
+    e6: 2,
     chosen_application: "",
     chosen_provider: "",
     custom_vm_list: null,
@@ -332,10 +331,16 @@ export default {
     vms: [
       {
         id: "1",
-        num_cpus: "5",
+        num_cpus: "2",
         mem_size: "100MB",
         disk_size: "200MB",
       },
+      {
+        id: "2",
+        num_cpus: "4",
+        mem_size: "200MB",
+        disk_size: "400MB",
+      }
     ],
     selected_vms: [],
     headers: [
@@ -361,6 +366,41 @@ export default {
   //     },
   //   },
   methods: {
+    generatePCPInput() {
+      let formData = new FormData();
+      let workflow_file = this.workflow_file[0];
+      formData.append("workflow_file", workflow_file);
+      const json = JSON.stringify(this.selected_vms)
+      const blob = new Blob([json], {
+        type: 'application/json'
+      })
+      formData.append("selected_vms", blob)
+
+      console.log("sending api request");
+      axios
+        .post("http://localhost:5001/generate", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data"
+          },
+        })
+        .then((res) => {
+          const url = window.URL.createObjectURL(new Blob([res.data]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", "pcp_input_template.yaml");
+          document.body.appendChild(link);
+          link.click();
+          console.log("Sucess");
+          this.dialog_input = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log("no response");
+          console.error(error);
+          this.dialog = true;
+        });
+
+    },
     getAvailableVms() {
       axios
         .get(`http://localhost:5001/get_vms/${this.chosen_provider}`)
