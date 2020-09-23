0e8b3084c7014570ec38e18f365c6b40eb8bf5f5
Robin <r.m.p.linde@students.uu.nl>
2020-09-23 14:09:43 +0200

    Clear formdata after successfull ajax requests

--- src/views/Plannerv3.vue
+++ src/views/Plannerv3.vue
@@ -70,7 +70,12 @@
               ></v-file-input>
               <v-divider class="mx-4" inset vertical></v-divider>
               <!-- <v-spacer></v-spacer> -->
-              <v-btn class="mt-2" color="primary" @click="loadVMList" :disabled="custom_vm_list === null">Load list</v-btn>
+              <v-btn
+                class="mt-2"
+                color="primary"
+                @click="loadVMList"
+                :disabled="custom_vm_list === null"
+              >Load list</v-btn>
             </v-toolbar>
           </template>
         </v-data-table>
@@ -88,17 +93,17 @@
       <v-stepper-content step="5">
         <!-- <p>Two planning algorithm were detected for your application type, configure the parameters below:</p> -->
         <!-- <p> Select preffered cloud provider </p> -->
-        <p> Do you want to generate a performance model? If you have your own you can skip this step.</p>
+        <p>Do you want to generate a performance model? If you have your own you can skip this step.</p>
         <v-btn class="justify-start" color="green" dark @click="getNumberOfTasks">Generate</v-btn>
         <v-flex xs12 offset-xs8>
-        <!-- <v-file-input
+          <!-- <v-file-input
           multiple
           label="Insert price model"
           accept=".yaml, .yml"
           v-model="pcp_price_model_file"
-        ></v-file-input> -->
-        <v-btn color="primary" @click="e6 = 6">Continue</v-btn>
-        <v-btn text @click="e6 = 4">Previous</v-btn>
+          ></v-file-input>-->
+          <v-btn color="primary" @click="e6 = 6">Continue</v-btn>
+          <v-btn text @click="e6 = 4">Previous</v-btn>
         </v-flex>
 
         <v-dialog v-model="dialog_input" max-width="490">
@@ -106,14 +111,18 @@
             <v-card-title class="headline">Customise</v-card-title>
 
             <v-card-text>We have detected {{number_of_tasks}} tasks in your workflow</v-card-text>
-            <v-switch class="mx-4" v-model="text_field1" label="Select custom performance values for cheapest vm"></v-switch>
+            <v-switch
+              class="mx-4"
+              v-model="text_field1"
+              label="Select custom performance values for cheapest vm"
+            ></v-switch>
             <v-text-field
-            class="mx-4"
-            v-model="performance_values"
-            :label="`Provide ${number_of_tasks.toString()} performance values seperated by comma`"
-            outlined
-            :disabled="text_field1 === false"
-          ></v-text-field>
+              class="mx-4"
+              v-model="performance_values"
+              :label="`Provide ${number_of_tasks.toString()} performance values seperated by comma`"
+              outlined
+              :disabled="text_field1 === false"
+            ></v-text-field>
             <v-card-actions>
               <v-spacer></v-spacer>
 
@@ -131,39 +140,36 @@
             <v-card-actions>
               <v-spacer></v-spacer>
 
-              <v-btn color="green darken-1" text @click="() => {dialog_input2 = false; e6 = 6}">Proceed</v-btn>
+              <v-btn
+                color="green darken-1"
+                text
+                @click="() => {dialog_input2 = false; e6 = 6}"
+              >Proceed</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
-
-
       </v-stepper-content>
 
       <v-stepper-step step="6">Configure QoS demands</v-stepper-step>
       <v-stepper-content step="6">
-          <v-file-input
+        <v-file-input
           multiple
-          label="Insert performance model"
+          label="Insert price and performance model"
           accept=".yaml, .yml"
           v-model="pcp_performance_file"
         ></v-file-input>
-        <!-- <v-form>
-          <v-container>
-            <v-row>
-              <v-col cols="12" sm="6" md="3">
-                <v-text-field
-                  label="Specify deadline for the entire workflow"
-                  placeholder="50"
-                  v-model="deadline"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-form> -->
+        <v-form>
+          <v-text-field
+            class="my-4"
+            label="Specify deadline for the entire workflow"
+            placeholder="50"
+            v-model="deadline"
+          ></v-text-field>
+        </v-form>
         <v-btn
           color="primary"
           @click="getToscaViaFiles"
-          :disabled="pcp_performance_file === null"
+          :disabled="(pcp_performance_file === null || deadline === 0)"
           :loading="loading"
         >Generate</v-btn>
         <v-btn text @click="e6 = 5">Previous</v-btn>
@@ -324,18 +330,20 @@ export default {
     dialog_input: false,
     dialog_input2: false,
     stepper_visible: true,
-    e6: 2,
+    e6: 1,
     chosen_application: "",
     chosen_provider: "",
     performance_values: "",
     custom_vm_list: null,
     workflow_file: null,
+    send_workflow: false,
     switch1: false,
     text_field1: false,
     number_of_tasks: 0,
     pcp_performance_file: null,
     pcp_price_model_file: null,
-    deadline: "",
+    formdata: null,
+    deadline: 0,
     radio_button_visible: false,
     activate_data_iterator: false,
     radio_value: "",
@@ -366,7 +374,7 @@ export default {
         num_cpus: "4",
         mem_size: "200MB",
         disk_size: "400MB",
-      }
+      },
     ],
     selected_vms: [],
     headers: [
@@ -392,21 +400,22 @@ export default {
   //     },
   //   },
   methods: {
-    getNumberOfTasks(){
-      let formData = new FormData();
+    getNumberOfTasks() {
       let workflow_file = this.workflow_file[0];
-      formData.append("workflow_file", workflow_file);
-       console.log("sending api request");
+      this.formdata.append("workflow_file", workflow_file);
+      console.log("sending api request");
       axios
-        .post("http://localhost:5001/get_tasks", formData, {
+        .post("http://localhost:5001/get_tasks", this.formdata, {
           headers: {
-            "Content-Type": "multipart/form-data"
+            "Content-Type": "multipart/form-data",
           },
         })
         .then((res) => {
-          this.number_of_tasks = res.data
+          this.number_of_tasks = res.data;
           console.log("res.data");
           this.dialog_input = true;
+          this.send_workflow = true;
+          this.formdata.delete("workflow_file")
         })
         .catch((error) => {
           // eslint-disable-next-line
@@ -414,23 +423,22 @@ export default {
           console.error(error);
           this.dialog = true;
         });
-
     },
     generatePCPInput() {
-      let formData = new FormData();
-      let workflow_file = this.workflow_file[0];
-      formData.append("workflow_file", workflow_file);
-      const json = JSON.stringify(this.selected_vms)
+      // let formData2 = new FormData();
+      // let workflow_file = this.workflow_file[0];
+      // formData.append("workflow_file", workflow_file);
+      const json = JSON.stringify(this.selected_vms);
       const blob = new Blob([json], {
-        type: 'application/json'
-      })
-      formData.append("selected_vms", blob)
+        type: "application/json",
+      });
+      this.formdata.append("selected_vms", blob);
 
       console.log("sending api request");
       axios
-        .post("http://localhost:5001/generate", formData, {
+        .post("http://localhost:5001/generate", this.formdata, {
           headers: {
-            "Content-Type": "multipart/form-data"
+            "Content-Type": "multipart/form-data",
           },
         })
         .then((res) => {
@@ -443,6 +451,7 @@ export default {
           console.log("Sucess");
           this.dialog_input = false;
           this.dialog_input2 = true;
+          this.formdata.delete("selected_vms")
         })
         .catch((error) => {
           // eslint-disable-next-line
@@ -450,9 +459,9 @@ export default {
           console.error(error);
           this.dialog = true;
         });
-
     },
     getAvailableVms() {
+      this.formdata = new FormData()
       axios
         .get(`http://localhost:5001/get_vms/${this.chosen_provider}`)
         .then((response) => {
@@ -489,16 +498,17 @@ export default {
       const l = this.loader;
       this[l] = !this[l];
 
-      let formData = new FormData();
-      let workflow_file = this.workflow_file[0];
-      let pcp_performance_file = this.pcp_performance_file[0];
-      formData.append("workflow_file", workflow_file);
-      formData.append("input_file", pcp_performance_file);
+      // let formData2 = new FormData();
+      if(!this.send_workflow){
+      this.formdata.append("workflow_file", this.workflow_file[0]);
+      }
+      this.formdata.append("input_file", this.pcp_performance_file[0]);
       console.log("sending api request");
       axios
-        .post("http://localhost:5001/upload", formData, {
+        .post(`http://localhost:5001/upload/${this.deadline}`, this.formdata, {
           headers: {
             "Content-Type": "multipart/form-data",
+
           },
         })
         .then((res) => {
@@ -508,6 +518,8 @@ export default {
           console.log("Sucess");
           this[l] = false;
           this.loader = null;
+          this.formdata.delete("workflow_file")
+          this.formdata.delete("input_file")
           // this.dialog = true;
         })
         .catch((error) => {
@@ -536,12 +548,12 @@ export default {
       // const l = this.loader;
       // this[l] = !this[l];
 
-      let formData = new FormData();
+      // let formData4 = new FormData();
       let vm_list_file = this.custom_vm_list;
-      formData.append("file", vm_list_file);
+      this.formdata.append("file", vm_list_file);
       console.log("sending api request");
       axios
-        .post("http://localhost:5001/load_vm_list", formData, {
+        .post("http://localhost:5001/load_vm_list", this.formdata, {
           headers: {
             "Content-Type": "multipart/form-data",
           },
@@ -549,7 +561,8 @@ export default {
         .then((res) => {
           console.log(res.data);
           console.log("Sucess");
-          this.vms = res.data
+          this.vms = res.data;
+          this.formdata.delete("file")
           // this[l] = false;
           // this.loader = null;
           // this.dialog = true;
