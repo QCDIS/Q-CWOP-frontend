149209c02fab674d6916d5f6b25f2d23cb5e931f
skoulouzis <S.Koulouzis@uva.nl>
2020-09-29 23:12:16 +0200

    added proxy for CORS

--- vue.config.js
+++ vue.config.js
@@ -2,10 +2,10 @@ module.exports = {
   "configureWebpack": {
     "devtool": "source-map"
   },
+      devServer: {
+        proxy: 'http://localhost:5001'
+    },
   "transpileDependencies": [
     "vuetify"
   ]
-    devServer: {
-        proxy: 'http://localhost:5001'
-    }
 }
